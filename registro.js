c6ccb0fcfe63e55d6239f9e2e07b2a2c4ce12156
MaikB71 <49370727+MaikB71@users.noreply.github.com>
2021-09-28 06:20:07 -0500

    Create registro.js

---
+++ registro.js
@@ -0,0 +1,12 @@
+/*......... Team ...... */ 
+// Función Agregar Registro
+
+
+
+/*.........  ...... */ 
+// Función Ordenar Registro
+
+
+
+/*......... Michael- - ...... */ 
+// Función Filtar Correo
