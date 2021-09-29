6f1c67a7fb2b4c33c2ea8d521363d89bfd0d3d81
calipso4u <alex.hamey.iriarte@gmail.com>
2021-09-29 09:16:03 -0500

    Actualización Sprint 3

---
+++ registro.js
@@ -0,0 +1,26 @@
+const { agregarRegistro } = require("./form");
+
+
+function agregarRegistro(){
+    
+    let nombre = document.getElementById("nombre").value;
+    let apellidos = document.getElementById("apellidos").value;
+    let telefono = document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
+    let password = document.getElementById("password").value;
+    
+    let registro = {Nombre: nombre, Apellidos: apellidos, Teléfono: telefono, Correo: correo,  Contraseña: password};
+    var arreglo = arreglo.push(registro);
+    console.log("Los datos ingresados son:" <br> "Nombre: " + nombre <br> + "Apellidos: " + apellidos <br> + "Teléfono: " + telefono <br> + 
+    "Correo: " + correo <br> + "Contraseña: " + password);
+
+}
+function filtrarCorreo(arreglo){
+    resultado = arreglo.filter(resultado => /gmail.com/.test(arreglo.Correo) === true);
+    
+    return resultado 
+}
+
+
+
+module.exports = agregarRegistro;filtrarCorreo
