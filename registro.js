315a0122061af92eab3b239df3696a0e159dd7af
Davidluis96 <mrdavison96@gmail.com>
2021-09-30 08:00:42 -0500

    Update registro.js
    Se agrega las  funciones

--- registro.js
+++ registro.js
@@ -13,7 +13,34 @@ function agregarRegistro() {
 
 /*.........Daniel-David ...... */ 
 // Función Ordenar Registro
+let registro = [];
+function agregarRegistro(){
+	let datos ={
+		nombre: document.getElementById('nombre').value,
+		apellido: document.getElementById('apellidos').value,
+		telefono: document.getElementById('telefono').value,
+		correo: document.getElementById('correo').value,
+		contrasena: document.getElementById('password').value
+	}
+	registro.push(datos);
+}
 
+module.exports = {
+	agregarRegistro,
+}
+function ordenarArreglo(){
+	console.log(registro.sort(function (a, b) {
+		var apellido1 = a.apellido.toLowerCase();
+		var apellido2 = b.apellido.toLowerCase();
+		if (apellido1 < apellido2){
+			return -1;
+		}
+		if (apellido1 > apellido2){
+			return 1;
+		}
+		return 0;
+	}));
+}
 
 
 /*......... Michael-Alex-JuanF...... */ 
